db180e30cb34352219f926dfe229334bf45d7f37
FTC5 <dan.m1512@gmail.com>
2017-07-20 14:55:48 +0300

    Update SimpleNumbers.java

--- homework3.6/src/com/gmail/m1512/dan/SimpleNumbers.java
+++ homework3.6/src/com/gmail/m1512/dan/SimpleNumbers.java
@@ -4,10 +4,18 @@ public class SimpleNumbers {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for (int i = 0; i < 100; i++) {
-			if (i % 2 != 0) {
+		int a;
+		a=0;
+		for (int i = 1; i < 100; i++) {
+			for(int j=1;j<=i;j++) {
+				if(i%j==0) {
+					a++;
+				}
+			}
+			if(a<=2) {
 				System.out.print(i + " , ");
 			}
+			a=0;
 		}
 	}
 
