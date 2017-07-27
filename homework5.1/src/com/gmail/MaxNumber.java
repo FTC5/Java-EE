e8fdfbc16786f25cbf68a6626a984ad3a64293e6
FTC5 <dan.m1512@gmail.com>
2017-07-27 16:50:00 +0300

    Add files via upload

--- homework5.1/src/com/gmail/MaxNumber.java
+++ homework5.1/src/com/gmail/MaxNumber.java
@@ -6,23 +6,25 @@ public class MaxNumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int []arr=new int[10];
+		int[] arr = new int[10];
 		rand(arr);
 		System.out.println(Arrays.toString(arr));
 		System.out.println(max(arr));
-		
+
 	}
-	static void rand(int []arr) {
-		for(int i=0;i<arr.length;i++) {
-			arr[i]=(int)(Math.random()*100.0);
+
+	static void rand(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = (int) (Math.random() * 100.0);
 		}
-		
+
 	}
-	static int max(int []arr) {
-		int max=arr[0];
-		for(int i : arr) {
-			if(max<i) {
-				max=i;
+
+	static int max(int[] arr) {
+		int max = arr[0];
+		for (int i : arr) {
+			if (max < i) {
+				max = i;
 			}
 		}
 		return max;
