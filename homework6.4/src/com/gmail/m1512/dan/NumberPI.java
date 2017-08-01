e1ee555e6706ba230abc6b9329e652edd32bd5b0
FTC5 <dan.m1512@gmail.com>
2017-08-01 16:40:07 +0300

    Update NumberPI.java

--- homework6.4/src/com/gmail/m1512/dan/NumberPI.java
+++ homework6.4/src/com/gmail/m1512/dan/NumberPI.java
@@ -6,10 +6,10 @@ public class NumberPI {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for (int i = 2; i < 11; i++) {
+		for (int i = 2; i <= 11; i++) {
 			Formatter form = new Formatter();
 			form.format("%." + i + "f", Math.PI);
-			System.out.println(form.toString());
+			System.out.println(i-1+")"+form.toString());
 			form.close();
 		}
 	}
